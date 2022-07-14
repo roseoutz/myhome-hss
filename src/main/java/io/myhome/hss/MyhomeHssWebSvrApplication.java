21e6a2ede29a93731e89a7433c29d3b93fed487c
roseoutz <roseoutz@gmail.com>
2022-07-14 23:43:45 +0900

    myhome 청약 api서버 init

---
+++ src/main/java/io/myhome/hss/MyhomeHssWebSvrApplication.java
@@ -0,0 +1,13 @@
+package io.myhome.hss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeHssWebSvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhomeHssWebSvrApplication.class, args);
+    }
+
+}
